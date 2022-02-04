588aa41f9a909647c828f053361f035a6f764941
AlaynaGarry <Alaynagarry@gmail.com>
2022-02-04 12:00:15 -0700

    state agent

---
+++ Assets/Scripts/StateAgent/StateAgent.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateAgent : Agent
+{
+    public AgentPath path;
+    public StateMachine stateMachine = new StateMachine();
+
+
+    void Start()
+    {
+        stateMachine.AddState(new IdleState(this, "idle"));
+        stateMachine.AddState(new PatrolState(this, "patrol"));
+        stateMachine.SetState(stateMachine.StateFromName("idle"));
+    }
+
+    void Update()
+    {
+        stateMachine.Update();
+
+        if (movement.velocity.magnitude > 0.5f)
+        {
+            animator.SetBool("walk", true);
+        }
+        else
+        {
+            animator.SetBool("walk", false);
+        }
+
+    }
+    private void OnGUI()
+    {
+        GUI.Label(new Rect(10, 10, 100, 20), stateMachine.GetStateName());
+
+    }
+}
---
+++ Assets/Scripts/StateAgent/Transition.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Transition
+{
+	public enum Predicate
+	{
+		EQUAL,
+		LESS,
+		GREATER
+	}
+	public abstract bool ToTransition();
+}
